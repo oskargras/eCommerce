bb711d77199e9715ca4acd86c4e241b358827b57
Oskar Gras <grasiu@gmail.com>
2017-07-30 17:11:16 +0200

    created home and about sites & views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+    ctx = locals()
+    return render(request, 'home.html', ctx)
+
+
+def about(request):
+    ctx = locals()
+    return render(request, 'about.html', ctx)
